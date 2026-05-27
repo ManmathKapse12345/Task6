d1b5b25f13ec60daf589535201f2e1fc8bbd4ecb
ManmathKapse12345 <manmathkapse9@gmail.com>
2026-05-27 18:44:02 +0530

    basic Inheritance Implementation
    i have implemented inheritance of vehicle on Car,bike and truck

--- Assets/Scripts/Vehicle.cs
+++ Assets/Scripts/Vehicle.cs
@@ -5,42 +5,43 @@ public class Vehicle : MonoBehaviour
     public bool isStarted = false;
     public bool isMoving = false;
     public bool isStopped = true;
+    public float fuel = 50.0f;
     public float speed = 5.0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        StartVehicle();
-    }
+    //void Start()
+    //{
+    //    StartVehicle();
+    //}
 
-    // Update is called once per frame
-    void Update()
-    {
-        //if (isStarted && !isMoving && !isStopped)
-        //{
-        //    StartVehicle();
-        //}
-        //if (isMoving && !isStopped)
-        //{
-        //    Move();
-        //}
-        //if (isStopped)
-        //{
-        //    StopVehicle();
-        //}
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    //if (isStarted && !isMoving && !isStopped)
+    //    //{
+    //    //    StartVehicle();
+    //    //}
+    //    //if (isMoving && !isStopped)
+    //    //{
+    //    //    Move();
+    //    //}
+    //    //if (isStopped)
+    //    //{
+    //    //    StopVehicle();
+    //    //}
 
-        if (isStarted)
-        {
-            StartVehicle();
-        }
-        if (isMoving)
-        {
-            Move();
-        }
-        if (isStopped)
-        {
-            StopVehicle();
-        }
-    }
+    //    if (isStarted)
+    //    {
+    //        StartVehicle();
+    //    }
+    //    if (isMoving)
+    //    {
+    //        Move();
+    //    }
+    //    if (isStopped)
+    //    {
+    //        StopVehicle();
+    //    }
+    //}
     public void StartVehicle()
     {
         Debug.Log("Vehicle is started");
@@ -55,28 +56,36 @@ public class Vehicle : MonoBehaviour
     public void Move()
     {
         Debug.Log("Vehicle is moved");
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        //if(gameObject.name == "Cylinder")
+        //{
+        //    transform.Translate(Vector3.up * Time.deltaTime * speed);
+        //}
+        //else
+        //{
+        //    transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        //}
+        //transform.Translate(Vector3.forward * Time.deltaTime * speed);
     }
 
-    public void OnMouseDown()
-    {
-        if (!isStarted)
-        {
-            isStarted = true;
-            isStopped = false;
-        }
-        else if (!isMoving)
-        {
-            isStarted = false;
-            isMoving = true;
-            isStopped = false;
-        }
-        else if (!isStopped)
-        {
-            isMoving = false;
-            isStopped = true;
-            isStarted = false;
-        }
+    //public void OnMouseDown()
+    //{
+    //    if (!isStarted)
+    //    {
+    //        isStarted = true;
+    //        isStopped = false;
+    //    }
+    //    else if (!isMoving)
+    //    {
+    //        isStarted = false;
+    //        isMoving = true;
+    //        isStopped = false;
+    //    }
+    //    else if (!isStopped)
+    //    {
+    //        isMoving = false;
+    //        isStopped = true;
+    //        isStarted = false;
+    //    }
 
-    }
+    //}
 }
